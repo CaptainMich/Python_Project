11fdd6b75c3d7b55b6a9e88ee98d0efc8ba030b3
CaptainMich <michele.salvo96@gmail.com>
2016-12-10 18:59:38 +0100

    Start project solution; add new project NumericTypes -> MathOperator.py

---
+++ StartWithPython/StartWithPython/NumericTypes/MathOperator.py
@@ -0,0 +1,26 @@
+#MATH OPERATOR
+
+print('\n\t\tMATH OPERATOR\t\t\n')
+
+print(3 + 4) 					# example of add
+print(12 / 5)					# example of division 
+print(18 // 4)					# example of division(without rest)
+print(8 % 5)					# example of reminder
+print(5 ** 3)					# example of power of number
+
+print(8 + 2 * 10)				# example of order of operations (1. not ordered)
+print(8 + (2 * 10))				# example of order of operations (2. ordere
+
+print(int('8') + 5)				# example of conversion
+print(float('8.5') + 5)			# example of conversion(2)
+
+x = 5							# use of variable(1)
+print(x)						# ...
+print(20 + x)					# ...
+
+y = 12							# use of variable(2)
+print(y / x)					# ...
+print(y - x + 6)				# ...
+
+
+
