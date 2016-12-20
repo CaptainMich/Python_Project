2b8bcb951848f86c5bc4c256f641ae0fefcebd42
CaptainMich <michele.salvo96@gmail.com>
2016-12-20 19:17:50 +0100

    Add new project Loops -> While_Loop.py

---
+++ StartWithPython/StartWithPython/Loops/While_Loop.py
@@ -0,0 +1,14 @@
+#WHILE
+	
+print('\n\t\tWHILE\t\t\n')
+
+variable = 0									# first example
+												# ...
+while variable < 5:								# ...
+	print("Spam")								# ...
+	variable += 1								# ...
+
+print('')
+
+while True:										# infinite loop
+	print('Doing Something')
