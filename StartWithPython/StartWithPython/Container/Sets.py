aaa584111d72fd77936ffa41c603aa1c70e57636
CaptainMich <michele.salvo96@gmail.com>
2016-12-20 19:45:04 +0100

    Add new project Container -> Sets.py

---
+++ StartWithPython/StartWithPython/Container/Sets.py
@@ -0,0 +1,19 @@
+#SETS 
+
+print('\n\t\tSETS\t\t\n')
+
+groceries = {'cereal', 'milk', 'starcrunch', 'beer', 'duct tape', 'lotion', 'beer'} # list that can't containe any duplicate... 
+print(groceries)                                                                    # ... in fact,print beer is print only once	
+
+if 'milk'in groceries:
+	print('You already have milk hoss!')
+else:
+	print('You need milk!')
+
+if 'sausages'in groceries:
+	print('You already have sausages hoss!')
+else:
+	print('You need sausages!')
+
+print('')
+
