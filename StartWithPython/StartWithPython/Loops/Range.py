e3b9308c35d3bf5e67e40f662678e7aa33a31eea
CaptainMich <michele.salvo96@gmail.com>
2016-12-20 19:13:52 +0100

    Add new project Loops -> Range.py

---
+++ StartWithPython/StartWithPython/Loops/Range.py
@@ -0,0 +1,19 @@
+#RANGE 
+
+print('\n\t\tRANGE\t\t\n')
+
+									
+for x in range(10):							# to make an action ('n') times
+	print("Pippo")							# ...
+
+print('')
+
+for x in range(5, 12):						# second example -->  (from 'x' to(,) 'y') 
+	print(x)								# ...
+
+print('')
+
+for x in range(10, 40, 5):					# third example -->  (from 'x' to(,) 'y' in steps of(,) 'z') 
+	print(x)								# ...
+
+print('')
