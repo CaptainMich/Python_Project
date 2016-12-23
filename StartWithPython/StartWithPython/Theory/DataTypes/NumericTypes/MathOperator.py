0b45f3102702667d681dbe8b8c698ba8c3050f3d
CaptainMich <michele.salvo96@gmail.com>
2016-12-23 11:58:11 +0100

    Some formal changes to improve readibility(3)

--- StartWithPython/StartWithPython/Theory/DataTypes/NumericTypes/MathOperator.py
+++ StartWithPython/StartWithPython/Theory/DataTypes/NumericTypes/MathOperator.py
@@ -1,26 +1,28 @@
-#MATH OPERATOR
+# -------------------------------------------------------------------------------------------------
+# MATH OPERATOR
+# -------------------------------------------------------------------------------------------------
 
-print('\n\t\tMATH OPERATOR\t\t\n')
+print('\n\t\tMATH OPERATOR\n')
 
-print(3 + 4) 					# example of add
-print(12 / 5)					# example of division 
-print(18 // 4)					# example of division(without rest)
-print(8 % 5)					# example of reminder
-print(5 ** 3)					# example of power of number
+print(3 + 4)                        # example of add
+print(12 / 5)                       # example of division 
+print(18 // 4)                      # example of division(without rest)
+print(8 % 5)                        # example of reminder
+print(5 ** 3)                       # example of power of number
 
-print(8 + 2 * 10)				# example of order of operations (1. not ordered)
-print(8 + (2 * 10))				# example of order of operations (2. ordere
+print(8 + 2 * 10)                   # example of order of operations (1. not ordered)
+print(8 + (2 * 10))                 # example of order of operations (2. ordered)
 
-print(int('8') + 5)				# example of conversion
-print(float('8.5') + 5)			# example of conversion(2)
+print(int('8') + 5)                 # example of conversion
+print(float('8.5') + 5)             # example of conversion(2)
 
-x = 5							# use of variable(1)
-print(x)						# ...
-print(20 + x)					# ...
+x = 5                               # use of variable(1)
+print(x)                            # ...
+print(20 + x)                       # ...
 
-y = 12							# use of variable(2)
-print(y / x)					# ...
-print(y - x + 6)				# ...
+y = 12                              # use of variable(2)
+print(y / x)                        # ...
+print(y - x + 6)                    # ...
 
 
 
