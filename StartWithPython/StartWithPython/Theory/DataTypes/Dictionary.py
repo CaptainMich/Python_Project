668c334c5840bedcf20d10adafe8dbd103b00a71
CaptainMich <michele.salvo96@gmail.com>
2017-05-31 18:26:57 +0200

    Improve dictionary.py readability

--- StartWithPython/StartWithPython/Theory/DataTypes/Dictionary.py
+++ StartWithPython/StartWithPython/Theory/DataTypes/Dictionary.py
@@ -4,15 +4,19 @@
 
 print('\n\t\tDICTIONARY\n')
 
-car = {'Ferrari': 'is beautiful and super fast car',        # define a dictionary 
+identity = {}                                               # define empty dictionary 
+identity['name'] = 'Bob'                                    # create keys by assignment
+identity['job'] = 'Car driver'                              # ...
+identity['age'] = 25                                        # ...
+
+print(identity,'\n')                                        # print the dictionary (first way)
+
+car = {'Ferrari': 'is beautiful car',                       # define a dictionary 
        'Lamborghini' : 'is incredible car',                 # = {'key..': 'value..',...}
        'Maserati' : 'is expensive car'}                     # N.B : more than one entry per key not allowed
                                                                                                                         
-print(car,'\n')                                             # print all things in car
-print('Ferrari', car['Ferrari'],'\n')                       # print the value of ['Ferrari']
-
 for k,v in car.items():                                     # k = key, v = value
-    print(k,v)                                              # print the dictionary
+    print(k,v)                                              # print the dictionary (second way)
 
 # -------------------------------------------------------------------------------------------------
 # WORK with DICTIONARY
@@ -20,12 +24,16 @@ for k,v in car.items():                                     # k = key, v = value
 
 print('\n\n\t\tWORK with DICTIONARY\n')
 
+print('Ferrari', car['Ferrari'],'\n')                       # print the value of ['Ferrari']
+
+car['Ferrari'] += ',it is also very fast'                   # add string to 'Ferrari' value
+print('Ferrari', car['Ferrari'],'\n')                       # ... 
+
 car['Maserati'] = 'is luxury car';                          # update existing element of a dict
 print('Maserati', car['Maserati'],'\n')                     # ...
 
 car['Mercedes'] = "is very comfortable car";                # add new element to a dict
 print(car,'\n')                                             # ...
-
 
 del car['Ferrari'];                                         # remove entry with key 'Name'
 print("After delete Ferrari, dictionary is:")               # ...
